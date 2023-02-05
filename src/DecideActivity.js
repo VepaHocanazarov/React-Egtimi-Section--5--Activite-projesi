712a976e21300744e3d50a80041708743987debd
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-05 16:51:15 +0300

    41. Konum Bilgisi

--- src/DecideActivity.js
+++ src/DecideActivity.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 
  class DecideActivity extends Component {
   render() {
+    window.navigator.geolocation.getCurrentPosition((position) => {
+
+        console.log(position);
+
+      },
+      (err) =>{
+     
+        console.log(err);
+
+      }
+      );
     return (
       <div>
         
