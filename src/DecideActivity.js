2c896503ecf63952f0d43a2e7e5cd768095d1bbf
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-05 15:42:24 +0300

    40. Proje Kurulumu

---
+++ src/DecideActivity.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class DecideActivity extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default DecideActivity;
