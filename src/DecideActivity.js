dddd209f3a6e00344ce1a8e11bc6c2ecc3f417b3
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-07 12:11:43 +0300

    42. Sayfa Yükleniyor yapıldı

--- src/DecideActivity.js
+++ src/DecideActivity.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
 
  class DecideActivity extends Component {
+
+  state ={
+    longitude:0,
+    error:""
+  }
   render() {
     window.navigator.geolocation.getCurrentPosition((position) => {
+
+      this.setState({
+        longitude:position.coords.longitude
+      })
 
         console.log(position);
 
@@ -11,10 +20,35 @@ import React, { Component } from 'react'
      
         console.log(err);
 
+        this.setState({
+          error:err.message
+        })
+
       }
       );
+
+      const{longitude,error} = this.state;
+
+      if(longitude !== 0 &&  !error){
+        return(
+           <div> 
+            Boylam : {longitude}
+
+           </div>
+        )
+      }
+
+      else if(longitude === 0 && error){
+        return(
+          <div>
+            Hata : {error}
+          </div>
+        )
+      }
     return (
       <div>
+
+       Loading...
         
       </div>
     )
